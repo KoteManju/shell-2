6f189196eab6b7f478bf9e69c0aea946ae9e3974
wxianfeng <wxianfeng@li390-146.members.linode.com>
2014-06-30 02:04:56 -0400

    install git

---
+++ centos/script/config.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+SRC=$(dirname $PWD)
---
+++ centos/script/install_git16.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+source config.sh
+cd $SRC
+tar -zvxf git-1.6.1.tar.gz
+cd git-1.6.1
+./configure --prefix=/usr/local
+make
+sudo make install
