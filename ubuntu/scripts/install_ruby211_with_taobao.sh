94eeb176a7a9c630e0c8f64d2f11791c902b83e8
wxianfeng <wang.fl1429@gmail.com>
2014-08-29 15:09:17 +0800

    ubuntu ruby 2.1.1

---
+++ ubuntu/scripts/install_ruby211_with_taobao.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# http://ruby-china.org/wiki/install_ruby_guide
+
+echo '[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"' >> ~/.bashrc
+source ~/.bashrc
+
+rvm pkg install readline libyaml
+
+sed -i 's!ftp.ruby-lang.org/pub/ruby!ruby.taobao.org/mirrors/ruby!' $rvm_path/config/db
+
+rvm install 2.1.1 
+
+# rvm reinstall ruby-2.1.1
+
+gem sources --remove https://rubygems.org/
+
+gem sources -a https://ruby.taobao.org/
